dc63842fa2ecb89fdfe6ab23b87ea6c2778b2472
AmitDey01 <93072082+AmitDey01@users.noreply.github.com>
2023-08-02 20:58:22 +0530

    update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
       <section className="restaurant-detailed-banner">
         <div className="text-center">
           <img
-            src="https://d1mzjggyz5012h.cloudfront.net/quickzu.com/covers/3bd8570f2d74094c_Screen-Shot-2021-03-10-at-3.08.11-PM.png"
+            src="https://img.freepik.com/free-photo/restaurant-interior_1127-3394.jpg?w=1060&t=st=1690987966~exp=1690988566~hmac=f4e228a42a9e20872eb42412cab1a3579bfbe006c2c4950add3439b9f7d4ba96"
             alt="Le Cafe"
             className="img-fluid cover"
           />
@@ -20,7 +20,7 @@ const Header = () => {
                 <div className="restaurant-detailed-header-left">
                   <img
                     className="img-fluid mr-3 float-left"
-                    src="https://d1mzjggyz5012h.cloudfront.net/quickzu.com/logos/655b1a2a825e03d2_lecafelogo.png?width=300"
+                    src="https://img.freepik.com/free-vector/illustration-people-sitting-cafe_23-2148245388.jpg?w=740&t=st=1690989753~exp=1690990353~hmac=832a071ae101a8cd9c0f72cf656fcda7a02ed4d1865872c00d8b1f425849e3d5"
                     alt="Le Cafe"
                   />
                   <h2 className="text-white">Le Cafe</h2>
